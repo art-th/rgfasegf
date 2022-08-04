527b0f80c1af2091c6f7d59f37a70a8ab7a69986
unknown <arthit90990@gmail.com>
2022-08-05 00:58:30 +0700

    new

--- tkinter.py
+++ tkinter.py
@@ -7,7 +7,7 @@ def h():
     print("hel")
     
 
-l1 = Label(text="hajfoiajiofdjaiofjdioafjioadfjiofjaoifjaiosdfjo")
+l1 = Label(text="hajfoiajgfdsgsdgdfsgsdgfsdgsdgfiofdjaiofjdioafjioadfjiofjaoifjaiosdfjo")
 l1.grid(row=1)
 bt=Button(command=hi)
 bt.grid(row=0,column=1)
